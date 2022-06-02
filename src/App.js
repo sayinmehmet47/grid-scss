f38089ef065b7d09a85d11c926b4994f4e73b5a9
Mehmet Sayin <sayinmehmet47@Mehmets-MacBook-Pro.local>
2022-06-02 23:56:29 +0200

    feature(grid-scss): add library for layout and navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1 className="text-primary">Cards</h1>
+      <h2 className="text-3-light-primary">Card2</h2>
+      <button className="btn-primary">Button</button>
+      <div className="card">
+        <h1 className="card-title">This is a card</h1>
+        <p className="card-body">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+          recusandae, <a href="#"> ipsam provident est suscipit</a> a autem iure
+          nobis asperiores iusto.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <h2 class="mb-2">Grid System</h2>
+      <div class="row gap-2 justify-center">
+        <div class="col-12-xs col-5-sm col-3-xl ">
+          <div class="card">
+            <h3 class="card-title">Hello, ninjas</h3>
+            <p class="card-body">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="col-12-xs col-5-sm col-3-xl">
+          <div class="card">
+            <h3 class="card-title">Hello, ninjas</h3>
+            <p class="card-body">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="col-12-xs col-5-sm col-3-xl">
+          <div class="card">
+            <h3 class="card-title">Hello, ninjas</h3>
+            <p class="card-body">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="col-12-xs col-5-sm col-3-xl">
+          <div class="card">
+            <h3 class="card-title">Hello, ninjas</h3>
+            <p class="card-body">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
